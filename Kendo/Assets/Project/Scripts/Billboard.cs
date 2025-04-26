28341b07c356324e2ee86b01eac41c3b6fe5940c
jaga-Nick <shrimp1059yt@gmail.com>
2025-04-27 08:10:32 +0900

    ああ

---
+++ Kendo/Assets/Project/Scripts/Billboard.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+
+
+/// <summary>
+/// 常にカメラの方を向くオブジェクト回転をカメラに固定
+/// </summary>
+public class Billboard : MonoBehaviour
+{
+
+    private void Start()
+    {
+        // 回転をカメラと同期させる
+        transform.position = Camera.main.transform.position;
+    }
+    void LateUpdate()
+    {
+        // 回転をカメラと同期させる
+        transform.rotation = Camera.main.transform.rotation;
+    }
+}
