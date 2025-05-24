b324ac7f1f4c7e39a4add3cc07f5c70e0ebb46c4
yuun9129 <yuun9129@gmail.com>
2025-05-24 12:20:58 +0900

    5/24 12:20 亀井　Bomに個数を

--- Kendo/Assets/Project/Scripts/Presenter/PlayerBom.cs
+++ Kendo/Assets/Project/Scripts/Presenter/PlayerBom.cs
@@ -6,8 +6,20 @@ public class PlayerBom : MonoBehaviour
     [SerializeField] private InputActionReference _bomAction;
     public static bool bom;
 
+    [SerializeField] private int maxBomCount = 3;
+    private int currentBomCount = 1;
+
+    public static PlayerBom Instance { get; private set; }
+
     private void Awake()
     {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+
         _bomAction.action.performed += OnBom;
         bom = false;
     }
@@ -22,10 +34,30 @@ public class PlayerBom : MonoBehaviour
 
     private void OnBom(InputAction.CallbackContext context)
     {
-        if (!bom)
+        if (!bom && currentBomCount > 0)
         {
             bom = true;
+            currentBomCount--;
             BulletManager.Instance.ClearAllBullets();
         }
+    }
+    // 外部からボムを追加する用
+    public void AddBom()
+    {
+        if (currentBomCount < maxBomCount)
+        {
+            currentBomCount++;
+        }
+    }
+    // 外部参照用のBom個数
+    public int GetBomCount()
+    {
+        return currentBomCount;
+    }
+
+    // UI等の表示リフレッシュ
+    public int GetMaxBomCount()
+    {
+        return maxBomCount;
     }
 }
