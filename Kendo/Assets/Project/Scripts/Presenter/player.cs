0c3586068786c8d86c1c36161bb7fac6127d8a4a
LAPTOP-B3ETC2GO\matsu <nicokarma2050@gmail.com>
2025-05-24 13:47:00 +0900

    05/24 13:46 石崎 プレイヤーの移動アニメーション追加

--- Kendo/Assets/Project/Scripts/Presenter/player.cs
+++ Kendo/Assets/Project/Scripts/Presenter/player.cs
@@ -7,12 +7,22 @@ public class player : MonoBehaviour
     [SerializeField] private InputActionReference _moveAction;
     [SerializeField] private float _moveSpeed = 5f;
 
+    //テクスチャ変更用
+    private Renderer rend;
+    [SerializeField] private float switchInterval = 0.3f;
+    private float timer = 0f;
+    [SerializeField] private Texture idleTexture;
+    [SerializeField] private Texture[] textures;
+    private int textureIndex = 0;
+
     private Vector2 _moveInput;
 
     private void Awake()
     {
         _moveAction.action.performed += OnMove;
         _moveAction.action.canceled += OnMove;
+        rend = GetComponent<Renderer>();
+        rend.material.mainTexture = idleTexture;
     }
 
     private void OnDestroy()
@@ -32,8 +42,24 @@ public class player : MonoBehaviour
         // 移動しているときだけ向きを変える
         if (move.sqrMagnitude > 0.01f)
         {
+            //移動中は炎が出てる2種類の画像を交互に切り替える
+            timer += Time.deltaTime;
+            if (timer >= switchInterval)
+            {
+                // テクスチャを交互に切り替える
+                textureIndex = 1 - textureIndex;
+                rend.material.mainTexture = textures[textureIndex];
+                timer = 0f;
+            }
+
             // キャラの向きを移動方向に変更
             transform.forward = move.normalized;
+        }
+        else
+        {
+            rend.material.mainTexture = idleTexture;
+            timer = 0f;
+            textureIndex = 0;
         }
 
         // 実際の移動処理
